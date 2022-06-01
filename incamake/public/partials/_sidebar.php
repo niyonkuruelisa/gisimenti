bb3587acd8d04d557acdf49ecf7051c3ead537e7
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2022-06-02 00:17:42 +0200

    Resolved ahabanza'

--- incamake/public/partials/_sidebar.php
+++ incamake/public/partials/_sidebar.php
@@ -1,6 +1,6 @@
 <ul class="sidebar navbar-nav">
     <li class="nav-item ">
-        <a class="nav-link" href="<?php echo URLROOT;?>">
+        <a class="nav-link" href="<?php echo URLROOT;?>/incamake">
         <i class="fas fa-fw fa-home"></i>
         <span>Ahabanza</span>
         </a>
